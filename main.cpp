3dd537510bf90f6fda27635041132d895089b596
wontae <83wontae@gmail.com>
2023-07-07 16:08:56 +0900

    바뀌었다. 몬지 모르겠다.

--- main.cpp
+++ main.cpp
@@ -2,21 +2,23 @@
 #include "Engine.h"
 #include "Player.h"
 #include "Monster.h"
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <map>
 
 using namespace std;
 
 int main()
 {
 	Engine* MyEngine = new Engine();
-	APlayer MyPlayer;
+
 	MyEngine->SpawnActor(new APlayer());
 	MyEngine->SpawnActor(new AMonster());
 
 	MyEngine->Run();
 
 	delete MyEngine;
-
-
 
 	return 0;
 }
